307cddeac8af7b3450c91355c511d8bd4c01292a
thack <thack@LAPTOP-0AEGE8A9>
2021-04-02 22:19:38 -0400

    I would say that There was a couple problems including how the program didn't include things like cmath in the program and that there were no Parentheses and needed to switch n2 to n3 which I stated in the program

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,22 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
+float average(int i1, int i2, int i3) {
+    //Bug 1 here we should add () for adding 3 scores
+    return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+    int n1 = 0, n2 = 0, n3 = 0;
+    cout << "This program calculates the average of three numbers." << endl;
+    cout << "First number: "; cin >> n1;
+    cout << "Second number: "; cin >> n2;
+    //Bug 2 Here we should read into n3 not into n2
+    cout << "Third number: "; cin >> n3;
 
-	float a = average(n1, n2, n3);
+    float a = average(n1, n2, n3);
+    cout << setprecision(0) << fixed << "The average is " << a << endl;
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
-
-	return 0;
+    return 0;
 }
